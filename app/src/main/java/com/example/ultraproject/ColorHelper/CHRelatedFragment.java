8cb7693cdb6f450dda2bcf7dc0e4f3dfec64fcd3
donchik <ddv032006@gmail.com>
2022-03-20 00:43:36 +0200

    ended xml for color helper

--- app/src/main/java/com/example/ultraproject/ColorHelper/CHRelatedFragment.java
+++ app/src/main/java/com/example/ultraproject/ColorHelper/CHRelatedFragment.java
@@ -3,22 +3,20 @@ package com.example.ultraproject.ColorHelper;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
-
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.ultraproject.NavigationFragment;
 import com.example.ultraproject.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
-public class CHRelatedFragment extends Fragment {
+public class CHRelatedFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,25 +34,30 @@ public class CHRelatedFragment extends Fragment {
         FrameLayout leftTriadic = view.findViewById(R.id.left_triadic);
         FrameLayout centerTriadic = view.findViewById(R.id.center_triadic);
         FrameLayout rightTriadic= view.findViewById(R.id.right_triadic);
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.fab);
 
         Button back_to_main = view.findViewById(R.id.back_colorHelper);
-        navigation(back_to_main);
+        back_to_main.setOnClickListener(this);
+        floatingActionButton.setOnClickListener(this);
 
         return view;
     }
 
-    @SuppressLint("NonConstantResourceId")
-    public void navigation(Button button) {
-        button.setOnClickListener(view -> {
-            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
-            if (view.getId() == R.id.back_colorHelper) {
-                transaction.setReorderingAllowed(true);
-                transaction.replace(R.id.MainActivity, NavigationFragment.class, null);
-                transaction.commit();
-            }
 
-        });
+    @Override
+    public void onClick(View view) {
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+        if (view.getId() == R.id.back_colorHelper) {
+            transaction.setReorderingAllowed(true);
+            transaction.replace(R.id.MainActivity, NavigationFragment.class, null);
+            transaction.commit();
+        }
+        if (view.getId() == R.id.fab){
+            transaction.setReorderingAllowed(true);
+            transaction.replace(R.id.MainActivity, AddColorFragment.class, null);
+            transaction.commit();
+        }
     }
 }
