f48ed8079c5df53c9491dae68c740f54dca05a2a
UltraMan <super-kolia2006@ukr.net>
2022-03-21 22:23:35 +0200

    upgrade search

---
+++ app/src/main/java/com/example/ultraproject/NotesReviewFragment.java
@@ -0,0 +1,22 @@
+package com.example.ultraproject;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotesReviewFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.fragment_notes, null);
+
+        return view;
+    }
+}
